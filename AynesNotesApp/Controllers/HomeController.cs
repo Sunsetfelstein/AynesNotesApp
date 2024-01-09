242c89466ffd6c1c288563fc43f9b0cd2cff248f
sharipov <sharipov@ascon-ufa.ru>
2024-01-09 17:31:24 +0500

    Add project files

---
+++ AynesNotesApp/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AynesNotesApp.Controllers;
+
+public class HomeController: Controller
+{
+    [Route("")]
+    public string Index()
+    {
+        return "Hello World!";
+    }
+}
